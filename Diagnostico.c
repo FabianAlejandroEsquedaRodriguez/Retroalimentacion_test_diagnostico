6c8283acd38497edf8bb9d66b845399e92ec6978
Fabián Alejandro Esqueda Rodríguez <fabian.esqueda2892@alumnos.udg.mx>
2020-09-20 12:27:23 -0500

    Ahora se modifico el codigo y se agrego una funcion para la opcion 1

--- Diagnostico.c
+++ Diagnostico.c
@@ -1,6 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//Funciones
+void arreglo_enteros(){
+    int enteros[5];
+    int suma = 0;
+    float promedio;
+
+    printf("Introduce numeros enteros al arreglo\n\n");
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("Posicion [%i]: ", i);
+        scanf("%i", &enteros[i]);
+    }
+    printf("\n\n");
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("[%i] ", enteros[i]);
+        //Suma iterativa
+        suma += enteros[i];
+    }
+    printf("\n\nLa suma es: %i", suma);
+    printf("\nEl promedio es: %.2f", promedio = suma/5);
+}
 
 int main(){
         char op;
@@ -13,12 +35,13 @@ int main(){
 
                 printf("\n\nElige una opcion: ");
                 scanf("%c", &op);
+                system("cls");
 
                 fflush(stdin);
 
                 switch (op)
                 {
-                case '1':
+                case '1': arreglo_enteros(); printf("\n\n"); system("pause"); system("cls");
                     break;
 
                 case '2':
